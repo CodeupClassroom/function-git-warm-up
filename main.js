ad01dcd8702ca61c9600fbf11032079f4ef7b89c
Justin Reich <justinreich.dev@gmail.com>
2018-10-08 08:36:14 -0500

    add code and readme

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+/* TO DO
+  Create a function, returnName() that takes in a string input and returns the string input.
+  Test the function by calling returnName(), passing in your first name. Assume the function
+  will only receive valid name inputs.
+
+  INPUT: returnName("Justin")  OUTPUT: "Justin"
+
+  You should console log to test your input.
+
+*/
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Function / Git Warm Up
+
+1. Clone down this repository (use the Terminal or IntelliJ) into your IdeaProjects directory
+1. Create a branch with your first and last name seperated by a dash (justin-reich)
+1. Follow the to-do steps in the main.js file
+1. Add and commit your work
+1. Push up your branch
+
+
